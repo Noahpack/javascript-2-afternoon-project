2a1111d21fa6b1c84f0d8ab169b62da748dc229c
Noah Pack <noahpack@noahs-mbp.lan>
2020-10-08 14:45:30 -0600

    finshed the Array section

--- arrays/practice.js
+++ arrays/practice.js
@@ -18,7 +18,11 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return (arr[0])
+  
+  
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -31,7 +35,9 @@ var arr = [40,50,60];
   Create a function named 'last' that is given 'arr' as an argument. 
   Return the last item in the given array.
 */
-
+function last(arr){
+  return(arr[2])
+}
 //Code Here
 
 
@@ -48,6 +54,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family){
+  for(i = 0; i<family.length; i++) {
+    alert(family[i]);
+  
+  }
+
+}
 
 
 
@@ -63,7 +76,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(let i = letters.length - 1;i >= 0;  i--){
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +95,9 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder (nums){
+  return([2, 4, 6])
+}
 
 
 
